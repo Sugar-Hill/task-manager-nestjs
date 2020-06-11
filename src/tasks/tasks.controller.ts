7456206262eb60b111232930377ed1ef99ee0bfa
Ayo ANIMASHAUN <ayofouad@gmail.com>
2020-06-11 20:15:39 +0100

    added filter method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDTO } from './dto/create-task.dto';
+import { SearchByTaskDTO } from './dto/search-by-task .dto';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {}
+
+    @Get()
+    searchByTaskDTO(@Query() searchByTaskDTO:SearchByTaskDTO): Task[] {
+        //check if there are parameters given
+        if(Object.keys(searchByTaskDTO).length) {
+            return this.tasksService.getTasksBySearchFilters(searchByTaskDTO);
+        } else {
+            return this.tasksService.getAllTasks();
+        }
+    }
 
     @Get()
     getAllTasks(): Task[] {
@@ -31,4 +42,6 @@ export class TasksController {
     patchTaskStatus(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
         return this.tasksService.patchTaskStatus(id, status);
     }
+
+
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,11 +2,35 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid} from 'uuid';
 import { CreateTaskDTO } from './dto/create-task.dto';
+import { SearchByTaskDTO } from './dto/search-by-task .dto';
 
 
 @Injectable()
 export class TasksService {
+
+
+
     private tasks:Task[] = [];
+
+    getTasksBySearchFilters(searchByTaskDTO: SearchByTaskDTO): Task[] {
+
+        //extract the variables from searchbytaskdto
+        const { searchTerm, status } = searchByTaskDTO;
+
+        //get all the tasks and put it in a variable
+        let allTasks = this.getAllTasks();
+
+        //if the status is provided filter by status
+        if(status){
+            allTasks = this.tasks.filter(task => task.status === status); 
+        }
+        //if a searchterm is provided filter by the searchterm, check if the searchterm is included in both the description and the title
+        if(searchTerm){
+            allTasks = this.tasks.filter(task => task.title.includes(searchTerm) || task.description.includes(searchTerm)); 
+        }
+        //return the tasks
+        return allTasks;
+    }
 
     getAllTasks(): Task[]{
         return this.tasks;
