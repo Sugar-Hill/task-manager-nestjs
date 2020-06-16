c5ffff27dfe362c6d8f820729009027f75aab38a
Fouad Animashaun <ayofouad@gmail.com>
2020-06-16 09:22:12 +0100

    implemented signup modules and added password hashing

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('/signup')
+    signUp(@Body(ValidationPipe) authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
+        return this.authService.signUp(authCredentialsDTO);
+    }
+}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,33 @@
+import { Repository, EntityRepository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+@EntityRepository(User)
+export class  UserRepository extends Repository<User>{
+    async signUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
+        const { username, password } = authCredentialsDTO;
+
+        const salt = await bcrypt.genSalt();
+
+        const user = new User();
+        
+        user.username = username;
+        user.salt = salt;
+        user.password = await this.hashPassword(password, user.salt);
+
+        try {
+            await user.save();
+        } catch (error) {
+            if(error.code == 23505){
+                throw new ConflictException('Username already exists');
+            }else{
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+
+    private async hashPassword(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password, salt);
+    }
+}
